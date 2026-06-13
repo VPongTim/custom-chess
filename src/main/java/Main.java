3a1ece1189b1776dbca6895b8864c2159a46e030
Tim Härcke <thaercke@gmail.com>
2026-06-14 00:04:08 +0200

    Minor fixes to pathclear - check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,42 +13,53 @@ public class Main {
     public static void main(String[] args) {
 
         // checking basic functionality
-        Position pos1 = new Position(1, 3);
-        Position pos2 = new Position(2, 4);
-        Position pos3 = new Position(0, 0);
-        Position pos4 = new Position(7, 6);
-        Position pos5 = new Position(0, 4);
-        Position pos6 = new Position(5, 5);
-        Position newPos = new Position(3, 3);
-        
-        Pawn pwn = new Pawn(PieceColor.WHITE);
-        Rook rk = new Rook(PieceColor.BLACK);
-        Knight kn = new Knight(PieceColor.WHITE);
-        Bishop b = new Bishop(PieceColor.BLACK);
-        King k = new King(PieceColor.WHITE);
-        Queen q = new Queen(PieceColor.BLACK);
+        System.out.println("=== Path Blocking Tests ===");
 
-        Board board = new Board();
-        board.placePiece(pwn, pos2);
-        board.placePiece(rk, pos1);
-        board.placePiece(kn, pos3);
-        board.placePiece(b, pos4);
-        board.placePiece(k, pos5);
-        board.placePiece(q, pos6);
+        // 1. Rook horizontal, no blocker -> true
+        Board board1 = new Board();
+        board1.placePiece(new Rook(PieceColor.WHITE), new Position(0, 0));
+        System.out.println("Rook horizontal no blocker, expected true: "
+                + board1.movePiece(new Position(0, 0), new Position(5, 0)));
 
-        board.printBoard();
+        // 2. Rook horizontal, blocker between -> false
+        Board board2 = new Board();
+        board2.placePiece(new Rook(PieceColor.WHITE), new Position(0, 0));
+        board2.placePiece(new Pawn(PieceColor.BLACK), new Position(3, 0));
+        System.out.println("Rook horizontal blocked, expected false: "
+                + board2.movePiece(new Position(0, 0), new Position(5, 0)));
 
-        Position fMove1 = new Position(0, 4);
-        System.out.println(board.movePiece(pos5, fMove1));
-        board.printBoard();
+        // 3. Bishop diagonal, no blocker -> true
+        Board board3 = new Board();
+        board3.placePiece(new Bishop(PieceColor.WHITE), new Position(0, 0));
+        System.out.println("Bishop diagonal no blocker, expected true: "
+                + board3.movePiece(new Position(0, 0), new Position(4, 4)));
 
-        Position fMove2 = new Position(1, 6);
-        System.out.println(board.movePiece(pos1, fMove2));
-        board.printBoard();
+        // 4. Bishop diagonal, blocker between -> false
+        Board board4 = new Board();
+        board4.placePiece(new Bishop(PieceColor.WHITE), new Position(0, 0));
+        board4.placePiece(new Pawn(PieceColor.BLACK), new Position(2, 2));
+        System.out.println("Bishop diagonal blocked, expected false: "
+                + board4.movePiece(new Position(0, 0), new Position(4, 4)));
 
-        Position fMove3 = new Position(1, 2);
-        System.out.println(board.movePiece(pos3, fMove3));
-        board.printBoard();
+        // 5. Queen vertical, no blocker -> true
+        Board board5 = new Board();
+        board5.placePiece(new Queen(PieceColor.WHITE), new Position(4, 0));
+        System.out.println("Queen vertical no blocker, expected true: "
+                + board5.movePiece(new Position(4, 0), new Position(4, 6)));
+
+        // 6. Queen diagonal, blocker between -> false
+        Board board6 = new Board();
+        board6.placePiece(new Queen(PieceColor.WHITE), new Position(0, 0));
+        board6.placePiece(new Pawn(PieceColor.BLACK), new Position(3, 3));
+        System.out.println("Queen diagonal blocked, expected false: "
+                + board6.movePiece(new Position(0, 0), new Position(5, 5)));
+
+        // 7. Knight jumps over blocker -> true
+        Board board7 = new Board();
+        board7.placePiece(new Knight(PieceColor.WHITE), new Position(0, 0));
+        board7.placePiece(new Pawn(PieceColor.WHITE), new Position(1, 1));
+        System.out.println("Knight jumps over blocker, expected true: "
+                + board7.movePiece(new Position(0, 0), new Position(1, 2)));
 
         
     }
