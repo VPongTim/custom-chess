df42eb6d73cf7e413b342f1fde2f45558478498f
Tim Härcke <thaercke@gmail.com>
2026-06-15 03:03:08 +0200

    Add standard board initiliazation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,14 +13,6 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
 
-        game.getBoard().placePiece(new Pawn(PieceColor.WHITE), new Position(0, 1));
-        game.getBoard().placePiece(new Pawn(PieceColor.BLACK), new Position(0, 6));
-
-        System.out.println(game.getCurrentTurn()); // WHITE
-        System.out.println(game.move(new Position(0, 1), new Position(0, 2))); // true
-        System.out.println(game.getCurrentTurn()); // BLACK
-        System.out.println(game.move(new Position(0, 2), new Position(0, 3))); // false, wrong turn
-        System.out.println(game.move(new Position(0, 6), new Position(0, 5))); // true
-        System.out.println(game.getCurrentTurn()); // WHITE  
+        game.getBoard().printBoard();  
     }
 }
--- src/main/java/chess/Game.java
+++ src/main/java/chess/Game.java
@@ -8,6 +8,7 @@ public class Game {
     public Game() {
         board = new Board();
         currentTurn = PieceColor.WHITE;
+        standardBoardInit();
     }
 
     public Board getBoard() {
@@ -41,5 +42,42 @@ public class Game {
         else {
             currentTurn = PieceColor.WHITE;
         }
+    }
+
+    public void standardBoardInit() {
+        //set up pawns for both sides
+        for (int i = 0; i <= 7; i++) {
+            Position curPos = new Position(i, 1);
+            board.placePiece(new Pawn(PieceColor.WHITE), curPos);
+        }
+        for (int i = 0; i <= 7; i++) {
+            Position curPos = new Position(i, 6);
+            board.placePiece(new Pawn(PieceColor.BLACK), curPos);
+        }
+        //rook setup
+        board.placePiece(new Rook(PieceColor.WHITE), new Position(0, 0));
+        board.placePiece(new Rook(PieceColor.WHITE), new Position(7, 0));
+        board.placePiece(new Rook(PieceColor.BLACK), new Position(0, 7));
+        board.placePiece(new Rook(PieceColor.BLACK), new Position(7, 7));
+
+        //knight setup
+        board.placePiece(new Knight(PieceColor.WHITE), new Position(1, 0));
+        board.placePiece(new Knight(PieceColor.WHITE), new Position(6, 0));
+        board.placePiece(new Knight(PieceColor.BLACK), new Position(1, 7));
+        board.placePiece(new Knight(PieceColor.BLACK), new Position(6, 7));
+
+        //bishop setup
+        board.placePiece(new Bishop(PieceColor.WHITE), new Position(2, 0));
+        board.placePiece(new Bishop(PieceColor.WHITE), new Position(5, 0));
+        board.placePiece(new Bishop(PieceColor.BLACK), new Position(2, 7));
+        board.placePiece(new Bishop(PieceColor.BLACK), new Position(5, 7));
+
+        //queens setup
+        board.placePiece(new Queen(PieceColor.WHITE), new Position(3, 0));
+        board.placePiece(new Queen(PieceColor.BLACK), new Position(3, 7));
+
+        //kings setup
+        board.placePiece(new King(PieceColor.WHITE), new Position(4, 0));
+        board.placePiece(new King(PieceColor.BLACK), new Position(4, 7));
     }
 }
