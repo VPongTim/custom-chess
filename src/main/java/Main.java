602e1155dc879dd9adec19f1000491f33d7fe234
Tim Härcke <thaercke@gmail.com>
2026-06-15 03:20:44 +0200

    Minor change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,12 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
 
-        game.getBoard().printBoard();  
+        game.getBoard().printBoard();
+
+        System.out.println(game.move(new Position(4, 1), new Position(4, 3))); // white pawn e2-ish to e4-ish
+        game.getBoard().printBoard();
+
+        System.out.println(game.move(new Position(4, 6), new Position(4, 4))); // black pawn e7-ish to e5-ish
+        game.getBoard().printBoard(); 
     }
 }
